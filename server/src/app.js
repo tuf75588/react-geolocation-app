9845ef9a24e6fc2fa4a60fdad177527aabca2289
Andrew Davis <atd285@gmail.com>
2019-03-18 20:47:19 -0400

    add cors to backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
 const connection = require('./db');
 require('dotenv').config();
 
@@ -8,6 +9,7 @@ const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
+app.use(cors());
 connection.create('entries');
 app.use(morgan('dev'));
 app.use(helmet());
